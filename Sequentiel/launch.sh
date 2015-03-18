06796b47785f2d7aa4e30dbafa4a174a937b9e72
sbksba <sbksba@gmail.com>
2015-03-18 17:57:03 +0100

    [MOD] modification du launch pour correspondre a celui en MPI

--- Sequentiel/launch.sh
+++ Sequentiel/launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./bin/NBODY_direct --in=/users/Enseignants/fortin/Public/HPC_fev2015/Projet/data/plummer_128.nemo --tend=10.0 --dt=0.1 --soft=0.01 --sum --save
+./bin/NBODY_direct --in=/users/Enseignants/fortin/Public/HPC_fev2015/Projet/data/baredisk_8192-plummer_8192.nemo --tend=10.0 --dt=0.1 --soft=0.01 --sum --save
