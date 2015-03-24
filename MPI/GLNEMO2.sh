11aedcdc12db9b218837c6f2f3f4e4874d8b96c0
sbksba <sbksba@gmail.com>
2015-03-24 12:35:28 +0100

    [MOD] correction des scripts

--- MPI/GLNEMO2.sh
+++ MPI/GLNEMO2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /tmp/NBODY_direct_results_LSDev8Grp2/
+ls -lhtr |cut -d' ' -f10 > list
 cd ~fortin/Public/HPC_fev2015/Projet/NEMO/nemo_cvs/bin/
 ./glnemo2 /tmp/NBODY_direct_results_LSDev8Grp2/results_plummer_128.nemo_0.nemo
 cd ~
---
+++ MPI/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /tmp/NBODY_direct_results_LSDev8Grp2/
+rm *
+cd ~2900825/cours/M1S2/HPC/Projet/HPC-Project/MPI/logs/
+rm *
+cd ~2900825/cours/M1S2/HPC/Projet/HPC-Project/MPI/
