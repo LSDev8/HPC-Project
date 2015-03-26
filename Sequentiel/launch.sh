04fd7e616c92c7f06258b1ab9284fcab7b49fa67
sbksba <sbksba@gmail.com>
2015-03-26 19:07:13 +0100

    [MOD] correction du launch.sh

--- Sequentiel/launch.sh
+++ Sequentiel/launch.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+cd /tmp/NBODY_direct_results_LSDev8Grp2/
+rm -f *
+cd ~2900825/cours/M1S2/HPC/Projet/HPC-Project/Sequentiel
 ./bin/NBODY_direct --in=/users/Enseignants/fortin/Public/HPC_fev2015/Projet/data/$1 --tend=10.0 --dt=0.1 --soft=0.01 --sum --save
