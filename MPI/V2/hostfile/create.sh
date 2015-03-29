b70f7d3eca9974a574d943a6b1886abf5630d88b
SbkSba <sbksba@gmail.com>
2015-03-29 22:37:23 +0200

    [DEL] suppression de la Version 2

--- MPI/V2/hostfile/create.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in 8 4 2
-do
-    cat hostfile-301-16 | head -n $i > hostfile-301-0$i
-    cat hostfile-303-16 | head -n $i > hostfile-303-0$i
-    cat hostfile-308-16 | head -n $i > hostfile-308-0$i
-done
