eeb5684f3df6207acb097d61a3783b2cc4c86b67
sbksba <sbksba@gmail.com>
2015-05-06 18:31:34 +0200

    [FIX] modification des droits

--- Sequentiel/launch.sh
+++ Sequentiel/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /tmp/NBODY_direct_results_LSDev8Grp2/
-rm -f *
+#rm -f *.nemo
 cd ~2900825/cours/M1S2/HPC/Projet/HPC-Project/Sequentiel
 ./bin/NBODY_direct --in=/users/Enseignants/fortin/Public/HPC_fev2015/Projet/data/$1 --tend=6.0 --dt=0.1 --soft=0.01 --sum --save
